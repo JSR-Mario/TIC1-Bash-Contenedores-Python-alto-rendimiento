eaddda7ce01854dde5f7968e1455a4a70e29bc90
JSRMario <juanmario@ciencias.unam.mx>
2025-06-30 21:08:36 -0600

    feat: agrega la estructura del diseño

--- top-words-1.sh
+++ top-words-1.sh
@@ -1,2 +1,14 @@
 #!/usr/bin/env bash
-curl -sl "https://www.gutenberg.org/files/11/11-0.txt" |tr '[:upper:]' '[:lower:]'| grep -oE "[a-z\']{2,}"| sort| grep -Fvwf stopwords| uniq -c | sort -nr| head -n10
+
+# Script para conseguir las primeras n palabras en un texto por frecuencia desde un archivo de texto, las regresa en minusculas
+# ARGS: NUM_WORDS: int default 10
+# OUTPUT: N palabras y sus frecuencias ordenadas al reverso
+
+NUM_WORDS="${1:-10}"
+tr '[:upper:]' '[:lower:]'|
+grep -oE "[a-z\']{2,}"| 
+sort| 
+grep -Fvwf stopwords| 
+uniq -c | 
+sort -nr| 
+head -n"${NUM_WORDS}"
